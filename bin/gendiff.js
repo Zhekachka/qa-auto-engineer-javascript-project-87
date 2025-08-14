52c4a92d7979f6060acc392b617e43e9acae0b0c
EvgeniyaKokareva <zhekachkakokareva@yandex.ru>
2025-08-14 22:14:44 +0300

    Блок №2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,4 +10,6 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version(pkg.version)
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv)
