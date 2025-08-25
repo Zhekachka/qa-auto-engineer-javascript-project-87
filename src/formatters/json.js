89a8bcb468e29d52bdcca8d9bbc0ffe99bfccfae
EvgeniyaKokareva <zhekachkakokareva@yandex.ru>
2025-08-25 11:25:07 +0300

    4. Формат json исправление

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,33 +1,4 @@
 // Преобразовываем в джисон формат
-const formatJson = (diff) => {
-  const result = {
-    removed: [], // для удаленных
-    added: [], // для добавленных
-    changed: [], // для измененных
-  }
-
-  diff.forEach((node) => { // обработка каждого параметра
-    switch (node.type) {
-      case 'removed':
-        result.removed.push(node.key) // только ключ
-        break
-      case 'added':
-        result.added.push({ [node.key]: node.value }) // ключ и значение
-        break
-      case 'changed':
-        result.changed.push({
-          [node.key]: { // историю
-            oldValue: node.oldValue, // до
-            newValue: node.newValue, // после
-          },
-        })
-        break
-      default:
-        break
-    }
-  })
-
-  return JSON.stringify(result, null, 2) // два пробела для отступов
-}
+const formatJson = (diff) => JSON.stringify(diff, null, 2)
 
 export default formatJson
